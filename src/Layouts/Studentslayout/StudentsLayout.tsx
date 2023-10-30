f516705d13427b12724f6b20e1eb0eae35601adc
sethumadhavan kochukrishnan <sethumadh@gmail.com>
2023-10-30 10:39:46 +1000

    modify ui for student navbar

--- src/Layouts/Studentslayout/StudentsLayout.tsx
+++ src/Layouts/Studentslayout/StudentsLayout.tsx
@@ -24,12 +24,9 @@ export default function StudentsNavbar() {
   const activeLink = location.pathname
   console.log(activeLink)
   return (
-    <div className="">
-      <div className="bg-gray-100 ">
-        <nav
-          className=" flex space-x-4 rounded-lg container px-4 sm:px-6 lg:px-8 py-4"
-          aria-label="Tabs"
-        >
+    <div className="hidden sm:block">
+      <div className=" container">
+        <nav className="-mb-px flex border-b border-gray-200 bg-slate-50" aria-label="Tabs">
           {tabs.map((tab) => (
             <NavLink
               end
@@ -38,9 +35,9 @@ export default function StudentsNavbar() {
               className={({ isActive }) => {
                 return cn(
                   activeLink.startsWith(tab.href) || isActive
-                    ? "bg-blue-500 text-white rounded-md px-3 py-2 text-sm font-medium underline underline-offset-4"
-                    : "text-gray-500 hover:text-gray-700 rounded-md px-3 py-2 text-sm font-medium",
-                  ""
+                    ? "border-indigo-500 text-indigo-600"
+                    : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                  "w-1/4 border-b-2 py-4 px-1 text-center text-sm font-medium"
                 )
               }}
               aria-current={tab.current ? "page" : undefined}
